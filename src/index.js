05f9612b68ce01c9db0963aa089973ee8e526ce3
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-10-30 17:42:58 +0200

    delete old part of code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,8 +1,6 @@
-import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { createSlice } from '@reduxjs/toolkit';
-import { nanoid } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './contactsOperations';
 
 const initialState = {
@@ -53,13 +51,6 @@ export const contactsSlice = createSlice({
 
   },
 });
-
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
 
 export const contactsReducer = contactsSlice.reducer;
 
